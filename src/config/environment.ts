94418252853eea8f1816fec2a67b3fd79e7f918d
rcrivolotti <roygabriel.crivolotti@mercadolibre.com>
2022-07-10 23:42:06 -0300

    Created config + Updated tsconfig

--- src/config/environment.ts
+++ src/config/environment.ts
@@ -1,15 +1,21 @@
+import { ENV as DevConfig } from "./dev";
+import { ENV as ProdConfig } from "./prod";
+
 export interface IEnv {
     PORT: number;
 }
 
 export const GetEnvironment: () => IEnv = () => {
+    let config: IEnv;
+
     if (process.env.ENVIRONMENT === "dev") {
-        let env = require("./dev");
-        return env;
+        config = DevConfig;
     } else if (process.env.ENVIRONMENT === "production") {
-        let env = require("./prod");
-        return env;
+        config = ProdConfig;
     } else {
-        return new Error('Shell variable ENVIRONMENT not set. Acceptable values are dev | production.');
+        console.error('Shell variable ENVIRONMENT not set: acceptable values are dev | production.');
+        process.exit(1);
     }
+
+    return config;
 };
---
+++ src/config/prod.ts
@@ -0,0 +1,5 @@
+import { IEnv } from "./environment";
+
+export const ENV: IEnv = {
+    PORT: 8080,
+}
